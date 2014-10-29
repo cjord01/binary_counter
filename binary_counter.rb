eab3872fbcc7d9f1cbecc77862043d22da1a2cf2
Clayton Jordan <clayjchicago@gmail.com>
2014-10-29 09:38:23 -0500

    refactored

--- binary_counter.rb
+++ binary_counter.rb
@@ -1,17 +1,17 @@
 def count_binary(num)
 	binary_nums = []
 	(0..num).each do |number|
-		if number.to_s.split('').uniq.size <= 2
-			if number.to_s.split('').sort.uniq.join('').to_i <= 1
-				binary_nums << number
+		if number.to_s.include?("0") || number.to_s.include?("1")
+			if number.to_s.split('').uniq.size <= 2
+				if number.to_s.split('').sort.uniq.join('').to_i <= 1
+					binary_nums << number
+				end
 			end
 		end
 	end
-	counter = 0
-	binary_nums.each do |binary_num|
-		puts "#{counter} - #{binary_num}"
-		counter += 1
+	binary_nums.each_with_index do |num, index|
+		puts "#{index} - #{num}"
 	end
 end
 
-					
+count_binary(1111111)
